0295f4e3a03beea31498b0d18a538178ef924469
cegwan <cegwan0402@gmail.com>
2025-08-03 12:24:03 +0900

    feat : createTodo, delTodo, addTodo
    할 일 생성, 할 일 삭제, 할 일 추가 기능 구현

--- app.js
+++ app.js
@@ -2,26 +2,34 @@ let addBtn = document.getElementById("addTodo");
 let inputBox = document.getElementById("inputField");
 let todoList = document.getElementById("todos");
 
-//함수 분리
-const addTodo = () => {
-  if (inputBox.value !== "") {
-    //할 일 박스
-    const todo = document.createElement("div");
-    //체크박스
-    const checkbox = document.createElement("input");
-    //체크박스 타입 주기
-    checkbox.type = "checkbox";
-    //텍스트(할 일 내용)
-    const text = document.createElement("span");
-    //삭제버튼
-    const delBtn = document.createElement("button");
-    //삭제 텍스트 추가
-    delBtn.textContent = "삭제";
+// 할 일 생성 함수
+const createTodoElement = (content) => {
+  const todo = document.createElement("div");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
 
-    //입력 받은 내용 할 일 내용에 넣기
-    text.textContent = inputBox.value;
-    //입력창 비우기
+  const text = document.createElement("span");
+  text.textContent = content;
+
+  const delBtn = document.createElement("button");
+  delBtn.textContent = "삭제";
+  delBtn.addEventListener("click", () => todo.remove());
+
+  todo.appendChild(checkbox);
+  todo.appendChild(text);
+  todo.appendChild(delBtn);
+
+  return todo;
+};
+
+// 할 일 추가 함수(list)
+const addTodo = () => {
+  const value = inputBox.value.trim();
+  if (value !== "") {
+    const todoItem = createTodoElement(value);
+    todoList.appendChild(todoItem);
     inputBox.value = "";
+    return;
   }
   alert("할 일을 입력해주세요");
 };
